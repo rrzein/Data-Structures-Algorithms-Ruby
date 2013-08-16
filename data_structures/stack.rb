6afce2bf1f1df336f071f3203d28a7ae92813a3e
Ricky Zein <rrzein@gmail.com>
2013-08-16 13:20:25 -0700

    Added stack implementation

---
+++ data_structures/stack.rb
@@ -0,0 +1,40 @@
+# Implementation of a stack in Ruby.
+
+Node = Struct.new(:back, :front, :value)
+
+class Stack
+  def initialize
+    @pointer = nil
+    @size = 0
+  end
+
+  def push(value)
+    prior_node = ((@pointer) ? @pointer : nil)
+    node = Node.new(prior_node, value)
+    prior_node.front = node if prior_node
+    @size += 1
+    @pointer = node
+    @pointer.value
+  end 
+
+  def pop
+    prior_node = @pointer.back
+    prior_node.front = nil if prior_node
+    @size -= 1
+    @pointer = prior_node
+    @pointer.value
+  end
+
+  def top
+    @pointer.value
+  end
+
+  def size
+    @size
+  end 
+
+  def empty?
+    true if @size == 0
+  end
+
+end
